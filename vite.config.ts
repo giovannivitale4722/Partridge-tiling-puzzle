511af96582262d441899dceae22d273f062582fe
giovannivitale4722 <136027772+giovannivitale4722@users.noreply.github.com>
2025-07-14 18:24:44 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     open: true
   },
-  base: '/partridge-tiling-puzzle/',
+  base: '/Partridge-tiling-puzzle/',
   build: {
     outDir: 'dist',
     sourcemap: true
